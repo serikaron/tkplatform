5d55d1e268bb834c6d38a0db1bf2fb5f1b445a1a
serikaron <y6M-L5P-E4Y-DMF>
2023-02-07 14:24:49 +0800

    user logic register

--- user/logic.mjs
+++ user/logic.mjs
@@ -28,3 +28,25 @@ userRouter.get("/phone/:phone", ...handle(async (req, res) => {
         .findOne({phone: req.params.phone})
     res.onSuccess(user)
 }))
+
+function todayTimestamp() {
+    return Math.floor(new Date(new Date().toISOString().slice(0, 10).replaceAll("-", "/")).getTime() / 1000)
+}
+
+export function register({registerUser, inviter, config}) {
+    registerUser.member = {
+        expiration: todayTimestamp() + config.daysForRegister * 86400
+    }
+
+    if (inviter === undefined) {
+        return {
+            registerUser
+        }
+    }
+
+    const baseline = Math.max(inviter.member.expiration, todayTimestamp())
+    inviter.member.expiration = baseline + config.daysForInvite * 86400
+    return {
+        registerUser, inviter
+    }
+}
---
+++ user/logic.test.js
@@ -0,0 +1,79 @@
+'use strict'
+
+import {register} from "./logic.mjs";
+
+function todayTimestamp() {
+    return Math.floor(new Date(new Date().toISOString().slice(0, 10).replaceAll("-", "/")).getTime() / 1000)
+}
+
+describe('register', () => {
+    describe("without inviter", () => {
+        it("should only add member to register user", () => {
+            const result = register({
+                registerUser: {
+                    phone: "13333333333",
+                    password: "123456"
+                },
+                config: {
+                    daysForRegister: 6,
+                    daysForInvite: 3,
+                }
+            })
+            expect(result.registerUser).toStrictEqual({
+                phone: "13333333333",
+                password: "123456",
+                member: {
+                    expiration: todayTimestamp() + 6 * 86400,
+                }
+            })
+        })
+    })
+
+    describe("with inviter", () => {
+        describe("member expired", () => {
+            it("extend member from now", () => {
+                const result = register({
+                    registerUser: {
+                        phone: "13333333333", password: "123456"
+                    }, inviter: {
+                        phone: "13444444444", member: {expiration: todayTimestamp()}
+                    }, config: {
+                        daysForRegister: 7, daysForInvite: 2
+                    }
+                })
+                expect(result).toStrictEqual({
+                    registerUser: {
+                        phone: "13333333333", password: "123456",
+                        member: {expiration: todayTimestamp() + 7 * 86400}
+                    },
+                    inviter: {
+                        phone: "13444444444", member: {expiration: todayTimestamp() + 2 * 86400}
+                    }
+                })
+            })
+        })
+        describe("member not expired", () => {
+            it("extend member from expiration", () => {
+                const result = register({
+                    registerUser: {
+                        phone: "13333333333", password: "123456",
+                    },
+                    inviter: {
+                        phone: "13444444444", member: {expiration: todayTimestamp() + 86400}
+                    },
+                    config: {
+                        daysForRegister: 7, daysForInvite: 3
+                    }
+                })
+                expect(result).toStrictEqual({
+                    registerUser: {
+                        phone: "13333333333", password: "123456",
+                        member: {expiration: todayTimestamp() + 7 * 86400}
+                    }, inviter: {
+                        phone: "13444444444", member: {expiration: todayTimestamp() + 4 * 86400}
+                    }
+                })
+            })
+        })
+    })
+})
