6ae5bc56065e3d9981754619d5720779a6fdc326
serikaron <y6M-L5P-E4Y-DMF>
2023-02-10 00:12:08 +0800

    接口文档

--- api/src/stubs/captcha.mjs
+++ api/src/stubs/captcha.mjs
@@ -38,10 +38,83 @@ class Captcha {
     }
 }
 
+/**
+ * @swagger
+ * /v1/captcha/require:
+ *   post:
+ *     tags: ["captcha(图形码)"]
+ *     description: 获取图形码
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               phone:
+ *                 type: string
+ *                 example: "13333333333"
+ *
+ *     responses:
+ *       200:
+ *         description: 获取成功
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 0
+ *                 msg:
+ *                   type: string
+ *                   example: "success"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     captcha:
+ *                       type: string
+ *                       example: "html"
+ *
+ */
 captchaRouter.post('/require', ...handleWithoutAuth1(async (req) => {
     return await Captcha.require(req.body)
 }))
 
+/**
+ * @swagger
+ * /v1/captcha/verify:
+ *   post:
+ *     tags: ["captcha(图形码)"]
+ *     description: 验证图形码
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               phone:
+ *                 type: string
+ *                 example: "13333333333"
+ *               code:
+ *                 type: string
+ *                 example: "3gE9"
+ *
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 0
+ *                 msg:
+ *                   type: string
+ *                   example: "success"
+ *
+ */
 captchaRouter.post('/verify', ...handleWithoutAuth1(async (req) => {
     return await Captcha.verify(req.body)
 }))
--- api/src/stubs/user.mjs
+++ api/src/stubs/user.mjs
@@ -37,10 +37,137 @@ class User {
     }
 }
 
+/**
+ * @swagger
+ * /v1/user/register:
+ *   post:
+ *     tags: ["user(用户相关)"]
+ *     description: 用户注册
+ *     requestBody:
+ *       required: ["phone", "password"]
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               phone:
+ *                 type: string
+ *                 example: "13333333333"
+ *                 required: true
+ *               password:
+ *                 type: string
+ *                 example: "123456"
+ *                 required: true
+ *               inviter:
+ *                 type: object
+ *                 properties:
+ *                   phone:
+ *                     type: string
+ *                     example: "13444444444"
+ *
+ *     responses:
+ *       200:
+ *         description: 注册成功
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 0
+ *                 msg:
+ *                   type: string
+ *                   example: "success"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     accessToken:
+ *                       type: string
+ *                       example: "a jwt object"
+ *                     refreshToken:
+ *                       type: string
+ *                       example: "96defd9f-1a54-4cb8-b501-9076d8709074"
+ *       409:
+ *         description: 用户已存在
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: -100
+ *                 msg:
+ *                   type: string
+ *                   example: "用户已存在"
+ *
+ *
+ */
 userRouter.post('/register', ...handleWithoutAuth1(async (req) => {
     return await User.register(req.body)
 }))
 
+/**
+ * @swagger
+ * /v1/user/login:
+ *   post:
+ *     tags: ["user(用户相关)"]
+ *     description: 用户登录
+ *     requestBody:
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               phone:
+ *                 type: string
+ *                 example: "13333333333"
+ *                 required: true
+ *               password:
+ *                 type: string
+ *                 example: "123456"
+ *                 required: true
+ *
+ *     responses:
+ *       200:
+ *         description: 登录成功
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: 0
+ *                 msg:
+ *                   type: string
+ *                   example: "success"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     accessToken:
+ *                       type: string
+ *                       example: "a jwt object"
+ *                     refreshToken:
+ *                       type: string
+ *                       example: "96defd9f-1a54-4cb8-b501-9076d8709074"
+ *       404:
+ *         description: 用户名或密码错误
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 code:
+ *                   type: integer
+ *                   example: -200
+ *                 msg:
+ *                   type: string
+ *                   example: "用户名或密码错误"
+ *
+ *
+ */
 userRouter.post('/login', ...handleWithoutAuth1(async (req) => {
     return await User.login(req.body)
 }))
