2c3f43b5cfd71915a9b99d988ea9376ee231344d
serikaron <y6M-L5P-E4Y-DMF>
2023-02-09 16:00:12 +0800

    integration test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 rm docker-compose.yaml
-ln -s deploy.dockker-compose.yaml docker-compose.yaml
+cp deploy.dockker-compose.yaml docker-compose.yaml
 docker compose pull && docker compose stop && docker compose up
