ffaa2465e966cec28b7022eeddd758b99bf02c8a
serikaron <y6M-L5P-E4Y-DMF>
2023-02-09 14:27:56 +0800

    integration test

--- tests/integration/src/user.test.js
+++ tests/integration/src/user.test.js
@@ -22,6 +22,9 @@ describe('register and login', () => {
                 password: "123456"
             },
             verify: (response) => {
+                if (response.code !== 0) {
+                    console.log(response.toString())
+                }
                 expect(response.status).toBe(200)
                 expect(response.code).toBe(0)
             }
