a2c8680a1d512d538b66ef09432535bdb00de8d4
serikaron <y6M-L5P-E4Y-DMF>
2023-02-16 01:08:00 +0800

    automation

--- user/handlers/addSite.test.js
+++ user/handlers/addSite.test.js
@@ -7,24 +7,24 @@ import supertest from "supertest";
 import {TKResponse} from "../../common/TKResponse.mjs";
 import {simpleCheckResponse} from "../../tests/unittest/test-runner.mjs";
 
-async function runTest(
-    {
-        body,
-        headers,
-        tkResponse = TKResponse.Success()
-    }
-) {
-    const app = createApp()
-    setup(app, {
-        setup: {
-            setup: testDIContainer.setup([]),
-            teardown: testDIContainer.teardown([])
-        }
-    })
-
-    const response = await supertest(app)
-        .post("/v1/user/site")
-        .set(headers)
-        .send(body)
-    simpleCheckResponse(response, tkResponse.status, tkResponse.code, tkResponse.msg, tkResponse.data)
-}
+// async function runTest(
+//     {
+//         body,
+//         headers,
+//         tkResponse = TKResponse.Success()
+//     }
+// ) {
+//     const app = createApp()
+//     setup(app, {
+//         setup: {
+//             setup: testDIContainer.setup([]),
+//             teardown: testDIContainer.teardown([])
+//         }
+//     })
+//
+//     const response = await supertest(app)
+//         .post("/v1/user/site")
+//         .set(headers)
+//         .send(body)
+//     simpleCheckResponse(response, tkResponse.status, tkResponse.code, tkResponse.msg, tkResponse.data)
+// }
