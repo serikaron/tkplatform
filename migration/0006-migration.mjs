2170fcf83de6cb46f2387a41a18c4dcfa63f8c92
serikaron <y6M-L5P-E4Y-DMF>
2023-02-24 23:27:20 +0800

    migration

--- migration/0006-migration.mjs
+++ migration/0006-migration.mjs
@@ -1,5 +1,7 @@
 'use strict'
 
+export const version = "0006"
+
 export async function migrate(mongoClient) {
     await createIndexes(mongoClient)
     await modifyUserSitesIndexes(mongoClient)
@@ -13,5 +15,5 @@ async function createIndexes(mongoClient) {
 
 async function modifyUserSitesIndexes(mongoClient) {
     await mongoClient.site.db.collection("userSites").dropIndex("userId_1")
-    await mongoClient.site.db.collection("userSites").createIndex({userId:1})
+    await mongoClient.site.db.collection("userSites").createIndex({userId: 1})
 }
