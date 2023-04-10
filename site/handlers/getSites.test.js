3d497b6961820ae305f2033ee6290bbd1a27c8d4
serikaron <y6M-L5P-E4Y-DMF>
2023-04-10 17:51:55 +0800

    backend users

--- site/handlers/getSites.test.js
+++ site/handlers/getSites.test.js
@@ -10,11 +10,18 @@ import {setup} from "../setup.mjs";
 import {ObjectId} from "mongodb";
 
 describe.each([
-    {path: '/v1/sites', filter: {usingDisable: 1}},
-    {path: '/v1/backend/sites', filter: {}}
+    {
+        name: "normal query",
+        query: {offset: 10, limit: 10, keyword: "abc"},
+        dbArguments: {offset: 10, limit: 10, keyword: "abc"}
+    },
+    {
+        name: "empty query",
+        dbArguments: {offset: 0, limit: 50, keyword: null}
+    },
 ])
-("$path", ({path, filter}) => {
-    it("should get from db and return", async () => {
+("$name", ({query, dbArguments}) => {
+    test("get sites for user", async () => {
         const id = new ObjectId()
         const getSites = jest.fn(async () => {
             return [{_id: id, msg: "a fake sites data"}]
@@ -34,11 +41,65 @@ describe.each([
             teardown: testDIContainer.teardown([])
         })
 
+        const url = query === undefined ? '/v1/sites' : `/v1/sites?${new URLSearchParams(query)}`
         const response = await supertest(app)
-            .get(path)
+            .get(url)
         await simpleCheckTKResponse(response, TKResponse.Success({
             data: [{id: `${id}`, msg: "a fake sites data"}]
         }))
-        expect(getSites).toHaveBeenCalledWith(filter)
+        expect(getSites).toHaveBeenCalledWith(dbArguments.offset, dbArguments.limit, dbArguments.keyword)
+    })
+})
+
+describe.each([
+    {
+        name: "normal query",
+        query: {offset: 10, limit: 10},
+        dbArgs: {offset: 10, limit: 10}
+    },
+    {
+        name: "empty query",
+        dbArgs: {offset: 0, limit: 50}
+    }
+])
+("$name", ({query, dbArgs}) => {
+    test("get sites for backend", async () => {
+        const id = new ObjectId()
+        const getSitesForBackend = jest.fn(async () => {
+            return [{_id: id, msg: "a fake sites data"}]
+        })
+        const countSitesForBackend = jest.fn(async () => {
+            return 10
+        })
+
+        const app = createApp()
+        setup(app, {
+            setup: testDIContainer.setup([
+                (req, res, next) => {
+                    req.context = {
+                        mongo: {
+                            getSitesForBackend,
+                            countSitesForBackend
+                        }
+                    }
+                    next()
+                }
+            ]),
+            teardown: testDIContainer.teardown([])
+        })
+
+        const url = query === undefined ? '/v1/backend/sites' : `/v1/backend/sites?${new URLSearchParams(query)}`
+        const response = await supertest(app)
+            .get(url)
+        simpleCheckTKResponse(response, TKResponse.Success({
+            data: {
+                total: 10,
+                offset: dbArgs.offset,
+                limit: dbArgs.limit,
+                items: [{id: id.toString(), msg: "a fake sites data"}]
+            }
+        }))
+        expect(getSitesForBackend).toHaveBeenCalledWith(dbArgs.offset, dbArgs.limit)
+        expect(countSitesForBackend).toHaveBeenCalled()
     })
 })
