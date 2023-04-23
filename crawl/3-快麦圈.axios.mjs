0bf932334e96ff25c1734e11553c97b900d433f5
serikaron <y6M-L5P-E4Y-DMF>
2023-04-23 17:44:50 +0800

    站点余额

--- crawl/3-快麦圈.axios.mjs
+++ crawl/3-快麦圈.axios.mjs
@@ -1,7 +1,55 @@
 'use strict'
 
 import axios from "axios";
+import JSSoup from "jssoup"
+
+const Soup = JSSoup.default
 
 const inst = axios.create({
-
+    baseURL: "http://m.kmq999.com",
+    withCredentials: true,
+    headers: {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15"},
+    // proxy: {host: "localhost", port: 61715}
 })
+
+let cookies = []
+
+const setCookies = (headers) => {
+    cookies = headers["set-cookie"].map(x => {
+        return x.split(";")[0]
+    })
+}
+
+const getLoginToken = async () => {
+    const r = await inst.get('/login')
+    setCookies(r.headers)
+    const soup = new Soup(r.data)
+    return soup.find("head")
+        .findAll('meta')
+        .map(x => x.attrs)
+        .filter(x => x.name === "csrf-token")[0]
+        .content
+}
+
+const login = async (token) => {
+    const r = await inst.post("/login", {
+        _token: token,
+        phone: "13587021931",
+        password: "hxy197984"
+    }, {
+        headers: {
+            Cookie: cookies.join(";"),
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    })
+
+    setCookies(r.headers)
+    console.log(r.data)
+    const soup = new Soup(r.data)
+    return soup.find("head")
+}
+
+
+const loginToken = await getLoginToken()
+const redirect = await login(loginToken)
+console.log(redirect)
--- crawl/3-快麦圈.selenium.mjs
+++ crawl/3-快麦圈.selenium.mjs
@@ -15,7 +15,7 @@ options.addArguments('--disable-popup-blocking');
 options.addArguments('--disable-notifications');
 options.addArguments('--start-maximized');
 options.addArguments('--headless')
-options.addArguments(`--proxy-server=localhost:61715`)
+// options.addArguments(`--proxy-server=localhost:61715`)
 
 const driver = new Builder()
     .forBrowser('chrome')
@@ -24,14 +24,13 @@ const driver = new Builder()
 
 const getTokenForLogin = async () => {
     await driver.get("http://m.kmq999.com/login")
-    console.log(await driver.getPageSource())
-    // const page = await inst.get("/login")
-    // const soup = new Soup(page.data)
-    // return soup.find("head")
-    //     .findAll('meta')
-    //     .map(x => x.attrs)
-    //     .filter(x => x.name === "csrf-token")[0]
-    //     .content
+    const page = await driver.getPageSource()
+    const soup = new Soup(page)
+    return soup.find("head")
+        .findAll('meta')
+        .map(x => x.attrs)
+        .filter(x => x.name === "csrf-token")[0]
+        .content
 }
 
 const login = async (token) => {
@@ -44,7 +43,7 @@ const login = async (token) => {
     // console.log(r.data)
 }
 
-await getTokenForLogin()
+const token = await getTokenForLogin()
 // await login(await getTokenForLogin())
 
 await driver.quit()
