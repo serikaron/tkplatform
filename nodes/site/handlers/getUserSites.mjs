c344e38dbaa9025c0d219309b3a3de3beb4615cb
serikaron <y6M-L5P-E4Y-DMF>
2023-05-31 14:06:19 +0800

    sites

--- nodes/site/handlers/getUserSites.mjs
+++ nodes/site/handlers/getUserSites.mjs
@@ -6,17 +6,31 @@ import {replaceId} from "../../common/utils.mjs";
 export function routeGetUserSites(router) {
     router.get("/user/sites", async (req, res, next) => {
         const dbRes = await req.context.mongo.getUserSites(req.headers.id)
+        // console.log(`getUserSites, dbRes: ${JSON.stringify(dbRes)}`)
+        for (const userSite of dbRes) {
+            delete userSite.userId
+            userSite.setting.schedule.forEach(s => {
+                if (s.activated === undefined) {
+                    s.activated = false
+                }
+            })
+            const site = await req.context.mongo.getSite(userSite.site.id)
+            // console.log(`site: ${JSON.stringify(site)}`)
+            userSite.site = site
+        }
+        // console.log(`getUserSites, dbRes: ${JSON.stringify(dbRes)}`)
         res.tkResponse(TKResponse.Success({
-            data: dbRes === null ? [] :
-                dbRes.map(replaceId).map(x => {
-                    delete x.userId
-                    x.setting.schedule.forEach(s => {
-                        if (s.activated === undefined) {
-                            s.activated = false
-                        }
-                    })
-                    return x
-                })
+            data: dbRes === null ? [] : dbRes
+            // data: dbRes === null ? [] :
+            //     dbRes.map(replaceId).map(x => {
+            //         delete x.userId
+            //         x.setting.schedule.forEach(s => {
+            //             if (s.activated === undefined) {
+            //                 s.activated = false
+            //             }
+            //         })
+            //         return x
+            //     })
         }))
         next()
     })
