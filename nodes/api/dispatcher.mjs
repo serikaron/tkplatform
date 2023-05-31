b1c9c0674ab51e14282022e6c1bb7c365dd04f97
serikaron <y6M-L5P-E4Y-DMF>
2023-05-31 14:15:35 +0800

    api dispatch

--- nodes/api/dispatcher.mjs
+++ nodes/api/dispatcher.mjs
@@ -31,10 +31,35 @@ export function dispatchContext(req, res, next) {
 
 export async function dispatch(req, res, next) {
     console.log(`dispatch req: ${req.url}, extractedToken::${JSON.stringify(req.extractedToken)}`)
+    function getUrl() {
+        if (req.routeInfo.hasOwnProperty("service")
+            && req.routeInfo.service.hasOwnProperty("url")
+        ) {
+            if (typeof req.routeInfo.service.url === "function") {
+                console.log(`getUrl, is function`)
+                return req.routeInfo.service.url(req)
+            }
+            if (req.query !== null
+                && req.query !== undefined
+                && Object.keys(req.query).length > 0) {
+                const out = `${req.routeInfo.service.url}?${new URLSearchParams(req.query)}`
+                console.log(`getUrl, with query, ${out}`)
+                return out
+            } else {
+                const out = req.routeInfo.service.url
+                console.log(`getUrl, without query, ${out}`)
+                return out
+            }
+        }
+
+        console.log(`getUrl, origin, ${req.url}`)
+        return req.url
+    }
+
     const axiosConfig = {
         baseURL: req.routeInfo.service === undefined || req.routeInfo.service.baseURL === undefined ? getBaseURL(req.url) : req.routeInfo.service.baseURL,
         method: req.method,
-        url: req.routeInfo.service === undefined || req.routeInfo.service.url === undefined ? req.url : req.routeInfo.service.url,
+        url: getUrl(),
         headers: req.extractedToken !== undefined ? req.extractedToken : {}
     };
     console.log(`dispatch, ${JSON.stringify(axiosConfig)}`)
