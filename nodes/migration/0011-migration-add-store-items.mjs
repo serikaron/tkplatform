0fa9781242f7033dea37e17a98cd070c01dcdf0e
serikaron <y6M-L5P-E4Y-DMF>
2023-07-11 11:44:37 +0800

    fix tests

--- nodes/migration/0011-migration-add-store-items.mjs
+++ nodes/migration/0011-migration-add-store-items.mjs
@@ -1,40 +1,42 @@
 'use strict'
 
+import {ObjectId} from "mongodb";
+
 export const version = "0011"
 
 export async function migrate(mongoClient) {
     await mongoClient.payment.db
         .collection("memberItems")
         .insertMany([{
-            _id: ObjectId("642f925807fdf4a11a3fbaf5"),
+            _id: new ObjectId("642f925807fdf4a11a3fbaf5"),
             name: 'VIP周卡',
             days: 7,
             price: 1000,
             originalPrice: 1000,
             promotion: false
         }, {
-            _id: ObjectId("642f92b407fdf4a11a3fbaf8"),
+            _id: new ObjectId("642f92b407fdf4a11a3fbaf8"),
             name: 'VIP月卡',
             days: 30,
             price: 3000,
             originalPrice: 3000,
             promotion: false
         }, {
-            _id: ObjectId("642f92f307fdf4a11a3fbafa"),
+            _id: new ObjectId("642f92f307fdf4a11a3fbafa"),
             name: 'VIP季卡',
-            days: Long("90"),
-            price: Long("8500"),
-            originalPrice: Long("9000"),
+            days: 90,
+            price: 8500,
+            originalPrice: 9000,
             promotion: true
         }, {
-            _id: ObjectId("642f932607fdf4a11a3fbafc"),
+            _id: new ObjectId("642f932607fdf4a11a3fbafc"),
             name: 'VIP半年卡',
-            days: Long("180"),
-            price: Long("16200"),
-            originalPrice: Long("18000"),
+            days: 180,
+            price: 16200,
+            originalPrice: 18000,
             promotion: true
         }, {
-            _id: ObjectId("642f934307fdf4a11a3fbafe"),
+            _id: new ObjectId("642f934307fdf4a11a3fbafe"),
             name: 'VIP年卡',
             days: 360,
             price: 28800,
@@ -44,21 +46,21 @@ export async function migrate(mongoClient) {
     await mongoClient.payment.db
         .collection('riceItems')
         .insertMany([{
-            _id: ObjectId("642f938a07fdf4a11a3fbb01"),
+            _id: new ObjectId("642f938a07fdf4a11a3fbb01"),
             name: '30天米粒',
-            rice: Long("450"),
-            price: Long("4500"),
-            originalPrice: Long("4500"),
+            rice: 450,
+            price: 4500,
+            originalPrice: 4500,
             promotion: true
         }, {
-            _id: ObjectId("642f93a707fdf4a11a3fbb03"),
+            _id: new ObjectId("642f93a707fdf4a11a3fbb03"),
             name: '90天米粒',
             rice: 1350,
             price: 12800,
             originalPrice: 13500,
             promotion: true
         }, {
-            _id: ObjectId("642f93c907fdf4a11a3fbb05"),
+            _id: new ObjectId("642f93c907fdf4a11a3fbb05"),
             name: '180天米粒',
             rice: 2700,
             price: 24000,
--- nodes/migration/0015-migration-settings.mjs
+++ nodes/migration/0015-migration-settings.mjs
@@ -1,5 +1,7 @@
 'use strict'
 
+import {ObjectId} from "mongodb";
+
 export const version = "0015"
 
 export async function migrate(mongoClient) {
@@ -17,6 +19,34 @@ export async function migrate(mongoClient) {
             {
                 key: "claimDownLinePrice",
                 value: 10,
+            }
+        ])
+    await mongoClient.payment.db
+        .collection("commissionSetting")
+        .insertMany([
+            {
+                _id: new ObjectId("644390b7514f55501eb7c647"),
+                commissionType: 1,
+                level: 1,
+                peopleNumber: 0,
+                rate: 5,
+                rate1: 30,
+                rate2: 20,
+                rate3: 10
+            },
+            {
+                _id: new ObjectId("644390c7514f55501eb7c649"),
+                commissionType: 2,
+                level: 2,
+                peopleNumber: 10,
+                rate: 5
+            },
+            {
+                _id: new ObjectId("644390d5514f55501eb7c64b"),
+                commissionType: 2,
+                level: 3,
+                peopleNumber: 20,
+                rate: 8
             }
         ])
 }
