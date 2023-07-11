af66fa9bbeb4bbe93b8d197e81201915a25a5021
serikaron <y6M-L5P-E4Y-DMF>
2023-07-11 13:15:09 +0800

    store items

--- nodes/payment/handlers/getItems.mjs
+++ nodes/payment/handlers/getItems.mjs
@@ -1,13 +1,18 @@
 'use strict'
 
 import {TKResponse} from "../../common/TKResponse.mjs";
+import {formatMoney, replaceId} from "../../common/utils.mjs";
 
 function route(path, fnName, router) {
     router.get(path, async (req, res, next) => {
         const fn = req.context.mongo[fnName]
         const items = await fn()
         res.tkResponse(TKResponse.Success({
-            data: items
+            data: items.map(x => {
+                replaceId(x)
+                x.price = formatMoney(x.price)
+                return x
+            })
         }))
         next()
     })
